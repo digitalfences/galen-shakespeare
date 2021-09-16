624c187e561d4c9e42122c8242038a0aa32c54da
galen.emanuel.fencing@gmail.com <galen.emanuel.fencing@gmail.com>
2021-09-15 23:21:49 -0700

    Api call implemented
    basic fetch call in place

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+const baseURL= "https://shakespeare.podium.com/api/reviews"
 
-function App() {
+const App = () => {
+  const [reviews, setReviews] = useState(null);
+
+  useEffect(async () => {
+    fetch(
+      "https://shakespeare.podium.com/api/reviews",
+      {
+        headers: { "X-Api-Key": "H3TM28wjL8R4#HTnqk?c" },
+      }
+    ).then(response => response.json()).then(data =>
+      setReviews(data)
+    )
+  }, [])
+
+
   return (
     <div className="App">
-      Ye Olde Review Board
+      <div className="app_banner">
+        <h1>Ye Olde Culture-al Review Board</h1>
+        <h2>"All newes of the barde that be fit to print!"</h2>
+      </div>
+      <div className="reviews">
+        {reviews ? reviews[0].rating : <h2>Loading...</h2>}
+      </div>
     </div>
   );
 }
