9ead12de2fa52e087009d295cb1d532fea62b71c
galen.emanuel.fencing@gmail.com <galen.emanuel.fencing@gmail.com>
2021-09-17 10:37:53 -0700

    Final styling
    Font and page styling finalized

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  test("renders app component", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Loading.../i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("async call test", () => {
+    render(<App />);
+    
+  })
 });
--- src/components/Review.js
+++ src/components/Review.js
@@ -1,10 +1,24 @@
 import './Review.css'
 
 const Review = ({review}) => {
+    let date = new Date(review.publish_date);
+    date = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear()
+
+    let rating = Math.round(review.rating)
+    let stars = [0,0,0,0,0]
+    for (let i = 0; i < 5; i++) {
+        if (rating >= i) {
+            stars[i] = 1
+        }
+    }
     return (
         <div className="review">
             <div className="review__content">
                 <div className="review__rating">
+                    {stars.map((num) => {
+                        if(num == 0) {return <>&#9734;</> }
+                        else { return (<>&#9733;</>)};
+                    })}
                     {review.rating}    
                 </div> 
                 <div className="review__body">
@@ -14,7 +28,7 @@ const Review = ({review}) => {
                     -{review.author}
                 </div>
                 <div className="review__time">
-                    Penned upon {review.publish_date}
+                    Penned upon {date}
                 </div>
             </div>   
         </div>
